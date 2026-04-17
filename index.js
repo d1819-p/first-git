4f10f267920210e99e2fce5bf824e3a17ddaa17f
dhavalpanchal0511 <dhavalpanchal0511@gmail.com>
2026-04-17 10:10:49 +0530

    new branch commit on index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const age = 21;
 let name1 = "Dhaval Panchal";
 let role = "Devloper";
+let s = "1,00,000";
 
 function companyRole(){
-    console.log("Name"+name1  + "Role"+role);
-    
+    console.log("Name "+name1  + " Role"+role);
 }
+
+function newSalary(){
+    console.log(`Your Salary is ${s}`);   
+}
+companyRole(),newSalary()
